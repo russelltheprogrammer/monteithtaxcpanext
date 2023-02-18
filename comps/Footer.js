79c2ce164af3ecc87fccc4f77f674f2b2969416b
russelltheprogrammer <russelltheprogrammer@gmail.com>
2023-02-18 14:41:42 -0500

    add year date api

--- comps/Footer.js
+++ comps/Footer.js
@@ -1,9 +1,11 @@
 const Footer = () => {
+
+    const year = new Date().getFullYear();
     
     return ( 
         <div className="footer-container">
             <div className="gradient-container-bottom"></div>
-            <i className="fas fa-copyright"></i>Copyright 2022 - All Rights Reserved - RUSSELL MONTEITH CPA PLLC
+            <i className="fas fa-copyright"></i>Copyright {year} - All Rights Reserved - RUSSELL MONTEITH CPA PLLC
         </div>
      );
 }
