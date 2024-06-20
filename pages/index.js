3c877fadae14f1a39f6b3234f31c8ec6922dda24
Russell <russelltheprogrammer@gmail.com>
2024-06-20 11:49:43 -0400

    progress home

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
+import React, { useRef, useEffect } from 'react';
 import Image from 'next/image';
 import Contact from './contact';
-import * as React from 'react';
 import { styled } from '@mui/material/styles';
 import Button from '@mui/material/Button';
 
@@ -12,7 +12,7 @@ const CustomButton = styled(Button)({
   border: '1px solid',
   lineHeight: 1.5,
   backgroundColor: 'rgb(76, 128, 153)',
-  borderColor: '#0063cc',
+  borderColor: 'rgb(56, 108, 133)',
   fontFamily: [
     '-apple-system',
     'BlinkMacSystemFont',
@@ -27,30 +27,68 @@ const CustomButton = styled(Button)({
   ].join(','),
   '&:hover': {
     backgroundColor: 'rgb(109, 163, 188)',
-    borderColor: '#0062cc',
+    borderColor: 'rgb(89, 143, 168)',
     boxShadow: 'none',
   },
   '&:active': {
     boxShadow: 'none',
     backgroundColor: 'rgb(109, 163, 188)',
-    borderColor: '#005cbf',
+    borderColor: 'rgb(73, 126, 151)',
   },
   '&:focus': {
-    boxShadow: '0 0 0 0.2rem rgba(0,123,255,.5)',
+    boxShadow: '0 0 0 0.2rem rgba(93, 146, 171)',
   },
 });
 
 export default function Home() {
+
+  const contactRef = useRef(null);
+  const imageWrapperRef = useRef(null);
+
+  const scrollToContact = () => {
+    if (contactRef.current) {
+      contactRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
+  useEffect(() => {
+    const observerCallback = (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('logo-image-animation');
+        } else {
+          entry.target.classList.remove('logo-image-animation');
+        }
+      });
+    };
+
+    const observer = new IntersectionObserver(observerCallback, {
+      threshold: 0.5, // Adjust as needed
+    });
+
+    const currentRef = imageWrapperRef.current;
+    if (currentRef) {
+      observer.observe(currentRef);
+    }
+
+    return () => {
+      if (currentRef) {
+        observer.unobserve(currentRef);
+      }
+    };
+  }, []);
 
   return (
     <div className="home-content-container">
       <div className="home-intro-container">
           <br/>
             <h1>Welcome to RUSSELL MONTEITH CPA PLLC</h1>
-            <h3>A niche tax and technology accounting practice</h3>
+            <h3>A niche tax and technology business</h3>
           <br/>
           <span>
-            <CustomButton variant="contained" size="large" >SCHEDULE A FREE CONSULTATION TODAY</CustomButton>
+            <CustomButton variant="contained" size="large" onClick={scrollToContact}>
+              SCHEDULE A FREE CONSULTATION TODAY
+            </CustomButton>
           </span>
           <br/>
           <br/>
@@ -71,14 +109,17 @@ export default function Home() {
         <br />
         <br />
         <br />
+        <div ref={imageWrapperRef}>
           <Image 
             id="homepage-logo-img" 
             src="/homepagelogoimage.png" 
             alt="Home Page Logo" 
             width={760} 
-            height={200} 
+            height={200}
           />
+          </div>
           <Contact header={"homepage"} homepagelink={"homepage"} logo={"homepage"} />
+          <div ref={contactRef}></div>
     </div>
   )
 }
