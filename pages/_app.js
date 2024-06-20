9095063829e3ab65ae7fb8ef794a30e5c2451ce5
Russell <russelltheprogrammer@gmail.com>
2024-06-20 19:27:22 -0400

    styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import Layout from '../comps/Layout';
 import '../styles/globals.scss';
 import '../styles/navbar.scss';
+import '../styles/contact.scss';
+import '../styles/footer.scss';
 
 function MyApp({ Component, pageProps }) {
   return ( 
