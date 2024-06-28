86ee1fbf3bd4e6d39a8f22dd49f2ddf50af2f4a1
Russell <russelltheprogrammer@gmail.com>
2024-06-28 15:26:26 -0400

    add home css

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import Layout from '../comps/Layout';
 import '../styles/globals.scss';
 import '../styles/navbar.scss';
+import '../styles/home.scss';
 import '../styles/about.scss';
 import '../styles/contact.scss';
 import '../styles/footer.scss';
