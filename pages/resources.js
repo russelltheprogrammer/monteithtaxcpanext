17956ba90a02c36b209dc9578d211d34346ae553
Russell <russelltheprogrammer@gmail.com>
2024-07-23 13:03:09 -0400

    resources section

--- pages/resources.js
+++ pages/resources.js
@@ -1,43 +1,106 @@
+import { Divider, List, ListItem, ListItemText, Typography, Link } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const interFontFamily = createTheme({
+  typography: {
+    fontFamily: [
+      'Inter',
+      'sans-serif'
+    ].join(','),
+  },
+});
+
 const Resources = () => {
+
+  const resources = [
+    {
+      title: 'Internal Revenue Service',
+      subtitle: '',
+      link: 'http://www.irs.gov'
+    },
+    {
+      title: 'IRS Direct Pay - ',
+      subtitle: 'Pay IRS taxes directly via the web (no sign-up necessary)',
+      link: 'https://www.irs.gov/payments/direct-pay'
+    },
+    {
+      title: 'New York State Department Of Taxation and Finance',
+      link: 'https://www.tax.ny.gov/'
+    },
+    {
+      title: 'NYS Department Of Taxation and Finance Online Services - ',
+      subtitle: 'Create an online account and pay NYS taxes directly via the web',
+      link: 'https://www.tax.ny.gov/online/'
+    },
+    {
+      title: 'TaxProjection.com - ',
+      subtitle: 'Tax tool made by me to help my clients estimate their quarterly taxes',
+      link: 'http://www.taxprojection.com'
+    },
+    {
+      title: 'Verify My CPA License',
+      link: 'https://eservices.nysed.gov/professions/verification-search'
+    },
+    {
+      title: 'Github - ',
+      subtitle: 'My programming development applications & projects',
+      link: 'https://github.com/russelltheprogrammer'
+    },
+    {
+      title: 'LinkedIn - ',
+      subtitle: 'Connect With Me',
+      link: 'https://www.linkedin.com/in/russell-monteith-cpa-0a43975a/'
+    },
+  ];
     
   return ( 
-    <div className='section-container'>
+    <div className='container-resources'>
       <div className="topic-header">
         <h1>USEFUL RESOURCES</h1>
       </div>
-      <div className="container-resources">
-        <ul>
-          <li>
-            <a href="http://www.irs.gov" rel="noreferrer" target="_blank">Internal Revenue Service</a>
-          </li>
-          <li>
-            <a href="https://www.irs.gov/payments" rel="noreferrer" target="_blank">IRS Pay</a>  
-                        &nbsp;&nbsp;<i className="fas fa-long-arrow-alt-right"></i> Pay IRS taxes directly via the web (no sign-up necessary)
-          </li>
-          <li>
-            <a href="https://www.tax.ny.gov/" rel="noreferrer" target="_blank">New York State Department Of Taxation and Finance</a>
-          </li>
-          <li>
-            <a href="https://www.tax.ny.gov/online/" rel="noreferrer" target="_blank">NYS Department Of Taxation and Finance Online Services</a>
-                        &nbsp;&nbsp;<i className="fas fa-long-arrow-alt-right"></i> Create an online account and pay NYS taxes directly via the web
-          </li>
-          <li>
-            <a href="http://www.taxprojection.com" rel="noreferrer" target="_blank">TaxProjection.com</a>
-                        &nbsp;&nbsp;<i className="fas fa-long-arrow-alt-right"></i> Tax tool made by me to help my clients estimate their
-                        quarterly taxes
-          </li>
-          <li>
-            <a href="http://www.op.nysed.gov/opsearches.htm" rel="noreferrer" target="_blank">Verify My CPA License</a>
-          </li>
-          <li>
-            <a href="https://github.com/russelltheprogrammer" rel="noreferrer" target="_blank">Github</a>  
-                        &nbsp;&nbsp;<i className="fas fa-long-arrow-alt-right"></i> My web programming projects &amp; applications
-          </li>
-          <li>
-            <a href="https://www.linkedin.com/in/russell-monteith-cpa-0a43975a/" rel="noreferrer" target="_blank">LinkedIn</a>
-          </li>
-        </ul>
-      </div>
+      <List>
+        {resources.map((resource, index) => (
+          <div key={index}>
+            <Divider 
+              sx={{ 
+                width: {xs: '80%', xl: '70%'}, 
+                mx: 'auto', 
+                borderBottomWidth: '5px',
+              }} />
+            <ThemeProvider theme={interFontFamily}>
+              <ListItem 
+                sx={{ 
+                  width: {xs: '80%', xl: '70%'}, 
+                  mx: 'auto', 
+                  py: { xs: 3, sm: 2 } 
+                }}>
+                <ListItemText
+                  primary={
+                    <Typography variant="h6" color="textPrimary" sx={{ fontSize: '22px' }}>
+                      {resource.title}<span style={{ fontSize: '20px'}}>{resource.subtitle}</span>
+                    </Typography>
+                  }
+                  secondary={
+                    <Typography sx={{ fontSize: '20px' }}>
+                      <Link href={resource.link} target="_blank" rel="noopener">
+                    Read More
+                      </Link>
+                    </Typography>
+                  }
+                />
+              </ListItem>
+            </ThemeProvider>
+            {index === resources.length - 1 ? 
+              <Divider 
+                sx={{ 
+                  width: {xs: '80%', xl: '70%'}, 
+                  mx: 'auto', 
+                  borderBottomWidth: '5px',
+                }} /> 
+              : ''}
+          </div>
+        ))}
+      </List>
     </div>
   );
 }
